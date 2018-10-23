71f0067c3ea9560697fd90c3a155e0ca65582fc5
Jakub Ziembinski <ziembinski.j@gmail.com>
2018-10-23 21:58:15 +0200

    Added posting measurements to Google Cloud

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
 #include <Arduino.h>
 #include <pms.h>
-
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson-v5.13.3.h>
 
-Pmsx003 pms(D5, D6);
-
 const char* ssid = "No Password For Ya";
 const char* pass = "MightyP@ssw0rd";
 
-auto lastRead = millis();
+Pmsx003 pms(D5, D6);
+
+// SETUP
 
 void setupInternetConnection() {
-  WiFi.mode(WIFI_STA);
+    WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, pass);
 	while (WiFi.status() != WL_CONNECTED) {
     	delay(500);
     	Serial.println("Waiting for connection");
-
   }
 }
 
@@ -36,6 +34,30 @@ void setup() {
 	setupPMS();
 }
 
+// RUN CODE
+
+void postData(JsonObject &object) {
+    if (WiFi.status() == WL_CONNECTED) {
+        const char* url = "https://us-central1-air-quality-monitor-server.cloudfunctions.net/aqmDataReceiver";
+        const char* thumbprint = "695821299d57b0e2eb0c713640e8D9e4da16f230";
+		
+        char messageBuffer[300];
+		object.prettyPrintTo(messageBuffer, sizeof(messageBuffer));
+
+		HTTPClient http;
+		http.begin(url, thumbprint);
+		http.addHeader("Content-Type", "application/json");
+		int httpCode = http.POST(messageBuffer);
+        if(httpCode != HTTP_CODE_OK) {
+            Serial.println("Response code: " + httpCode);
+        }
+		http.end();
+	} else {
+        Serial.println("Lost connection");
+        delay(500);
+    }
+}
+
 void loop(void) {
 	const auto n = Pmsx003::Reserved;
 	Pmsx003::pmsData data[n];
@@ -43,14 +65,7 @@ void loop(void) {
 	Pmsx003::PmsStatus status = pms.read(data, n);
 
 	switch (status) {
-		case Pmsx003::OK:
-		{
-			Serial.println("_________________");
-			auto newRead = millis();
-			Serial.print("Wait time: ");
-			Serial.println(newRead - lastRead);
-			lastRead = newRead;
-
+		case Pmsx003::OK: {
 			const int N = 3;
 			u_int pmDataIndexes [N] = {Pmsx003::PM1dot0, Pmsx003::PM2dot5, Pmsx003::PM10dot0};
 
@@ -63,52 +78,17 @@ void loop(void) {
 				auto pmName = Pmsx003::dataNames[index];
 				auto pmMetric = Pmsx003::metrics[index];
 				auto pm = data[index];
-
-				Serial.print(pmName);
-				Serial.print(":\t");
-				Serial.print(pm);
-				Serial.print(" ");
-				Serial.println(pmMetric);
-
+                
 				measurements[pmName] = pm;
 			}
 
-			if (WiFi.status() == WL_CONNECTED) {
-				Serial.println("");
-				Serial.println("");
-				char JSONmessageBuffer[300];
-				JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-				Serial.println(JSONmessageBuffer);
-
-				HTTPClient http;
-				http.begin("http://localhost:3000/measurements");
-				http.addHeader("Content-Type", "application/json");
-				int httpCode = http.POST(JSONmessageBuffer);
-				String payload = http.getString();
-				Serial.print("HTTP CODE: ");
-				Serial.println(httpCode);
-				Serial.println("Payload:");
-				Serial.println(payload);
-				http.end();
-			}
-			
-			// For loop starts from 3
-			// Skip the first three data (PM1dot0CF1, PM2dot5CF1, PM10CF1)
-			// for (size_t i = Pmsx003::PM1dot0; i < n; ++i) { 
-			// 	Serial.print(data[i]);
-			// 	Serial.print("\t");
-			// 	Serial.print(Pmsx003::dataNames[i]);
-			// 	Serial.print(" [");
-			// 	Serial.print(Pmsx003::metrics[i]);
-			// 	Serial.print("]");
-			// 	Serial.println();
-			// }
+            postData(JSONencoder);
 			break;
 		}
 		case Pmsx003::noData:
 			break;
 		default:
-			Serial.println("_________________");
+			Serial.print("___ ERROR ___ :");
 			Serial.println(Pmsx003::errorMsg[status]);
 	};
 }
